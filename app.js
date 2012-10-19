0fef37a802061b66aeeca44b134a8cd1c62a4ce9
Larger One <inhortte@gmail.com>
2012-10-19 14:50:14 +0300

    learning jade is fun

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ var express = require('express')
 
 var app = express();
 
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.send(500, { error: 'Something blew up!' });
+  } else {
+    next(err);
+  }
+}
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -19,11 +32,16 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
   app.use(app.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
+  app.use(logErrors);
+  app.use(clientErrorHandler);
   app.use(express.errorHandler());
 });
 
